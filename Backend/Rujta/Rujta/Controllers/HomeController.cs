3baf4cc47742c36467d5d5f6240c1ca0e518cced
mohamed ahmad <amohamedahmad68@gmail.com>
2025-10-13 22:07:43 +0300

    add project rujta, and add 2 folders (backend-frontend)

---
+++ Backend/Rujta/Rujta/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rujta.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        
+    }
+}
