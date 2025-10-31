53332ed209ab7ce7725baa9aadf13fe8d1e6b0d0
loki <Abdohamdy4587gmail.com>
2025-10-31 17:02:01 +0200

    oreder in place

---
+++ Backend/Rujta/Rujta.Application/DTOs/CartItemDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rujta.Application.DTOs
+{
+    class CartItemDto
+    {
+        public int MedicineId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
