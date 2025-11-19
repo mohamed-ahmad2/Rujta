7d021e6fd973dbc1efa5cd89c1c714e01141654e
Youssef wael <youssefwael063@gmail.com>
2025-11-19 18:01:41 +0200

    Make the Report for admin i think there is some more updates

---
+++ Backend/Rujta/Rujta.Application/DTOs/ProfitDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rujta.Application.DTOs
+{
+    public class ProfitSummaryDto
+    {
+        public decimal TotalProfit { get; set; }
+        public decimal ProfitMargin { get; set; } // percentage
+        public List<ProfitProductDto> Products { get; set; } = new();
+    }
+
+    public class ProfitProductDto
+    {
+        public int MedicineId { get; set; }
+        public string MedicineName { get; set; } = string.Empty;
+        public decimal CostPrice { get; set; }
+        public decimal SellPrice { get; set; }
+        public decimal Profit { get; set; }
+    }
+
+}
