c0cb4ebedaa875973a2eedbb0655b8685f420513
mohamed ahmad <amohamedahmad68@gmail.com>
2025-12-01 15:49:04 +0200

    run monster

--- Frontend/rujta-frontend/vite.config.js
+++ Frontend/rujta-frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://localhost:5001",
+        target: "https://localhost:7065",
         changeOrigin: true,
         secure: false,
         cookieDomainRewrite: "localhost",
