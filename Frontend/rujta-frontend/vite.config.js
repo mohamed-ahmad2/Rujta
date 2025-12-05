702813d913791ca064f7be0ee6a8a995e150e3b7
loki <Abdohamdy4587gmail.com>
2025-12-05 19:17:48 +0200

    a cart for each user

--- Frontend/rujta-frontend/vite.config.js
+++ Frontend/rujta-frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://localhost:7065",
+        target:"https://localhost:7065",
 
         changeOrigin: true,
         secure: false,
